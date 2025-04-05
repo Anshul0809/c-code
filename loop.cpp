948b434d7387114d39959c67000115e30c559ce5
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-04-05 19:37:42 +0530

    loop new program

--- loop.cpp
+++ loop.cpp
@@ -12,11 +12,11 @@ statement \code;
 increment\decrement
 }while(conditon);
 */
-
+/*
 #include<iostream>
 using namespace std;
 int main()
-/*{
+{
     int a=1;
     do
     {
@@ -71,6 +71,7 @@ do
 }
 */
 //write a program to print the sum of the values between 1 to 10
+/*
 {
     int a=1 ,s=0;
     do
@@ -80,4 +81,67 @@ do
  a++;
     }while(a<=10);
     cout<<"total value="<<s<<endl;
+}
+*/
+//write a program to generate factorial
+
+#include<iostream>
+using namespace std;
+int main()
+/*
+{
+    int number,a=1;
+    cout<<"enter number"<<endl;
+    cin>>number;
+    do
+    {
+        a=a*number;
+        number--;
+    }while(number>=1);
+    cout<<a<<endl;
+}
+*/
+//while (while loop will exicute the statement if the condition is ture otherwise it will terminet the loopand it will also called entry loop)
+//syntex
+
+/*veriable initilize
+while(condition)
+{
+    statement\code;
+    increment\decrement;
+}
+*/
+//using while loop print 1to 10
+/*
+{
+  int a=1;
+  while(a<=10)
+  {
+      cout<<a<<endl;
+      a++;
+  }
+}
+*/
+/*
+{
+    int a=10;
+    while(a>=1)
+    {
+        cout<<a<<endl;
+        a--;
+    }
+}
+*/
+//write a program to find out the how many digits in given number
+{
+    int number ,a=0;
+    cout<<"enter a number"<<endl;
+    cin>>number;
+    while(number>0)
+    {
+        a++;
+        number=number/10;
+
+    }
+    cout<<"total digit="<<a<<endl;
 }
